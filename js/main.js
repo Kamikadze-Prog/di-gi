c8910d444b9312742999fd918bd9fc4d38afde37
Kamikadze-Prog <kamikadze19900@gmail.com>
2020-12-09 00:16:09 +0200

    add carusel for 8 block  and fixed line position for 4 blovk  mob version

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
-$(function(){
+$(document).ready(function() {
+    $("#owl-demo").owlCarousel({
+        navigation : false, // Show next and prev buttons
+        slideSpeed : 300,
+        paginationSpeed : 400,
+        singleItem:true
 
+        // "singleItem:true" is a shortcut for:
+        // items : 1,
+        // itemsDesktop : false,
+        // itemsDesktopSmall : false,
+        // itemsTablet: false,
+        // itemsMobile : false
+    });
 });
