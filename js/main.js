5f67bb6f355860e323eb6f2f3e6d36de431871c4
Kamikadze-Prog <kamikadze19900@gmail.com>
2020-12-09 19:08:44 +0200

    added carusel for intro block

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
 $(document).ready(function() {
-    $("#owl-demo").owlCarousel({
+    $("#owl-demo-1").owlCarousel({
+        navigation : false, // Show next and prev buttons
+        slideSpeed : 300,
+        paginationSpeed : 400,
+        singleItem:true
+
+        // "singleItem:true" is a shortcut for:
+        // items : 1,
+        // itemsDesktop : false,
+        // itemsDesktopSmall : false,
+        // itemsTablet: false,
+        // itemsMobile : false
+    });
+
+    $("#owl-demo-2").owlCarousel({
         navigation : false, // Show next and prev buttons
         slideSpeed : 300,
         paginationSpeed : 400,
