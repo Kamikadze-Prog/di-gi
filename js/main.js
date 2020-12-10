5cfb70906547b4991df1f70531419fa21396e7b6
Kamikadze-Prog <kamikadze19900@gmail.com>
2020-12-10 11:54:22 +0200

    fixed slider for media and add prev nex buttons

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
     $("#owl-demo-1").owlCarousel({
-        navigation : false, // Show next and prev buttons
+        navigation : true, // Show next and prev buttons
         slideSpeed : 300,
         paginationSpeed : 400,
-        singleItem:true
-
+        singleItem:true,
+        navigationText : true
         // "singleItem:true" is a shortcut for:
         // items : 1,
         // itemsDesktop : false,
@@ -17,9 +17,8 @@ $(document).ready(function() {
         navigation : false, // Show next and prev buttons
         slideSpeed : 300,
         paginationSpeed : 400,
-        singleItem:true
-
-        // "singleItem:true" is a shortcut for:
+        singleItem:true,
+         // "singleItem:true" is a shortcut for:
         // items : 1,
         // itemsDesktop : false,
         // itemsDesktopSmall : false,
